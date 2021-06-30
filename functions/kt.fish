25cab4d165993e280182d4aa3d523990c5c9511e
Shawn Sorichetti <shawns@ziprecruiter.com>
2021-06-30 15:22:08 -0400

    Initial commit of main functionality
    This is mostly in perfect working condition.

---
+++ functions/kt.fish
@@ -0,0 +1,33 @@
+function kt
+    set file $argv
+    if test -z "$file"
+        for dir in ~/.kube/configs ~/.ssh/kubeconfigs
+            if test ! -d "$dir"
+                continue
+            end
+
+            for cand in (ls "$dir")
+                if test -n "$KUBECONFIG" && test "$KUBECONFIG" = "$dir/$cand"
+                    echo "* $cand"
+                else
+                    echo "  $cand"
+                end
+            end
+        end
+        return
+    end
+
+    if test -f "$file"
+        set -gx KUBECONFIG="$file"
+        return
+    end
+
+    for dir in ~/.kube/configs ~/.ssh/kubeconfigs
+        if test -f "$dir/$file"
+            set -gx KUBECONFIG "$dir/$file"
+            return
+        end
+    end
+    echo "Invalid KUBECONFIG: cannot file '$file'" >&2
+end
+
